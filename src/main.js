e9f7d3bb5137ca3092d762704977e4675af1428a
s1lin <lin.shilei@outlook.com>
2018-08-18 01:36:02 +0800

    更新样式，删除Bootstrap原生支持

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
+// import BootstrapVue from 'bootstrap-vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
 import axios from 'axios'
 
-Vue.use(BootstrapVue);
+// Vue.use(BootstrapVue);
 Vue.use(ElementUI);
 
 axios.defaults.baseURL = basePath;
